385e4dd3345f96daa915e5e7622f23c1b9f38a9d
mmoya26 <mmoya18@icloud.com>
2021-03-15 21:54:51 -0500

    Add some basic CSS  to the page to display future books

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let myLibrary = [];
 
 // Cosntructor
 function Book(title, author, pages, read) {
-    this.title = tile
+    this.title = title
     this.author = author
     this.pages = pages
     this.read = read
@@ -13,3 +13,21 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+function displayBooks() {
+    myLibrary.forEach(book => {
+        console.log(book);
+    })
+}
+
+// Temporary books data
+let bookOne = new Book("Jersey Shore", "MTV", 365, true);
+let bookTwo = new Book("Another Book", "Author 2", 100, false);
+let bookThree = new Book("Book number three", "Author 3", 150, true);
+
+addBookToLibrary(bookOne);
+addBookToLibrary(bookTwo);
+addBookToLibrary(bookThree);
+
+
+displayBooks();
