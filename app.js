976be8131bcc3648307ad6c7b6a0ed3052ea05b4
mmoya26 <mmoya18@icloud.com>
2021-03-15 22:18:14 -0500

    Add book functionality to display books in the DOM

--- app.js
+++ app.js
@@ -15,9 +15,39 @@ function addBookToLibrary(book) {
 }
 
 function displayBooks() {
+    const booksContainer = document.querySelector('.books');
+    
     myLibrary.forEach(book => {
-        console.log(book);
-    })
+        // Creating elements and assign their respective data
+        let bookCardContainer = document.createElement('div');
+
+        let bookTitle = document.createElement('h3');
+        bookTitle.textContent = book.title;
+
+        let bookAuthor = document.createElement('p');
+        bookAuthor.textContent = `Book Author: ${book.author}`;
+
+        let bookPages = document.createElement('p');
+        bookPages.textContent = `Book Pages: ${book.pages}`;
+
+        let bookRead = document.createElement('p');
+        bookRead.textContent = `Have read: ${book.read}`
+
+        // Add classes to all of the created elements
+        bookCardContainer.classList.add('book-card');
+        bookTitle.classList.add('book-title');
+        bookAuthor.classList.add('book-author');
+        bookPages.classList.add('book-pages');
+        bookRead.classList.add('book-read');
+
+        // Appends elements to the card container then container is added to the DOM element that
+        // holds all of the cards
+        bookCardContainer.appendChild(bookTitle);
+        bookCardContainer.appendChild(bookAuthor);
+        bookCardContainer.appendChild(bookPages);
+        bookCardContainer.appendChild(bookRead);
+        booksContainer.appendChild(bookCardContainer);
+    });
 }
 
 // Temporary books data
