d616995de78cdc21d049ff2f56a8ea466d3811fc
mmoya26 <mmoya18@icloud.com>
2021-03-16 00:55:05 -0500

    remove specific book functionality  + add book functionality + some css and  html changes

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 let myLibrary = [];
 
+const booksContainer = document.querySelector('.books');
+
+const buttonFormContainer = document.querySelector('.new-book-form-container');
+
+const titleInput = document.getElementById('input-title');
+const authorInput = document.getElementById('input-author');
+const pagesInput = document.getElementById('input-pages');
+const readInput = document.getElementById('haveRead');
+
+const addBookButton = document.getElementById('add');
+addBookButton.addEventListener('click', (e) => {
+    addBookToLibrary(new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked ? true : false));
+    e.preventDefault();
+    clearInputs();
+    buttonFormContainer.classList.toggle('display-none');
+});
+
+const addNewBookButton = document.getElementById('add-new-book');
+addNewBookButton.addEventListener('click', (e) => {
+    buttonFormContainer.classList.toggle('display-none');
+});
+
 // Cosntructor
 function Book(title, author, pages, read) {
     this.title = title
@@ -9,14 +31,40 @@ function Book(title, author, pages, read) {
     this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${read ? 'already read' : 'have not read yet'}`
 }
 
+function clearInputs() {
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readInput.checked = false;
+}
+
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    displayBooks();
+}
+
+// Created to refresh the page
+function removeAllBooks() {
+    
+    while(booksContainer.firstChild) {
+        booksContainer.removeChild(booksContainer.firstChild);
+    }
+}
+
+function removeSpecificBook(title) {
+    let filteredBooks = myLibrary.filter(book => {
+        return book.title != title;
+    });
+
+    myLibrary = filteredBooks;
+    displayBooks();
 }
 
 function displayBooks() {
-    const booksContainer = document.querySelector('.books');
-    
+
+    removeAllBooks();
+
     myLibrary.forEach(book => {
         // Creating elements and assign their respective data
         let bookCardContainer = document.createElement('div');
@@ -32,6 +80,20 @@ function displayBooks() {
 
         let bookRead = document.createElement('p');
         bookRead.textContent = `Have read: ${book.read}`
+        
+        // Remove button
+        let removeBookButton = document.createElement('button');
+        removeBookButton.textContent = "Remove Book";
+        removeBookButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            let card = e.target.parentElement;
+            let container = card.parentElement;
+
+            let title = card.querySelector('h3').textContent;
+            removeSpecificBook(title);
+            
+            container.removeChild(card);
+        })
 
         // Add classes to all of the created elements
         bookCardContainer.classList.add('book-card');
@@ -39,6 +101,7 @@ function displayBooks() {
         bookAuthor.classList.add('book-author');
         bookPages.classList.add('book-pages');
         bookRead.classList.add('book-read');
+        removeBookButton.classList.add('remove-book');
 
         // Appends elements to the card container then container is added to the DOM element that
         // holds all of the cards
@@ -46,18 +109,7 @@ function displayBooks() {
         bookCardContainer.appendChild(bookAuthor);
         bookCardContainer.appendChild(bookPages);
         bookCardContainer.appendChild(bookRead);
+        bookCardContainer.appendChild(removeBookButton);
         booksContainer.appendChild(bookCardContainer);
     });
 }
-
-// Temporary books data
-let bookOne = new Book("Jersey Shore", "MTV", 365, true);
-let bookTwo = new Book("Another Book", "Author 2", 100, false);
-let bookThree = new Book("Book number three", "Author 3", 150, true);
-
-addBookToLibrary(bookOne);
-addBookToLibrary(bookTwo);
-addBookToLibrary(bookThree);
-
-
-displayBooks();
